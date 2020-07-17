3b30960baf19bd44a0acfdf694461b13f872ca77
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2020-07-17 13:45:03 -0500

    [README] Update readme, add some seeds

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Tellie Fast Facts
 
+Fast Facts game app for interview process.
+
 ## Setup
 
 - `bundle install`
 - `bin/rails db:setup`
 - `bin/rails s`
+
+View at [http://localhost:3000](http://localhost:3000)
 
 ## Testing
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Episode.all.destroy_all
+
+(0...4).each do |i|
+  episode = Episode.create!(name: "Episode #{i}")
+
+  (0..4).each do |j|
+    episode.episode_statements.create!(statement: "Statement #{j}", correct: j == 0)
+  end
+end
